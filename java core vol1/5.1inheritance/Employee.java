feb3cb04cb4bd07b9e7a21219fbe24bd8aec1b9f
yuanzh <yuanzh@yuZh.local>
2020-02-29 23:24:05 +0800

    改名

---
+++ java core vol1/5.1inheritance/Employee.java
@@ -0,0 +1,23 @@
+public class Employee
+{
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void raiseSalary(double bypercent){
+        double raise = salary * bypercent / 100;
+        salary += raise;
+    }
+    private void getPrivate(){
+        return;
+    }
+}
---
+++ java core vol1/5.1inheritance/Person.java
@@ -0,0 +1,9 @@
+public abstract class Person
+{
+    public abstract String getDescription();
+    private String name;
+    public Person(String name)
+    {
+        this.name = name;
+    }
+}
